6ce20d2facc0b691457d43a700b464d6c8ced0be
Genrikh Antipov <genrikhantipov@gmail.com>
2024-11-07 18:05:21 +0300

    feat: add file input drag n drop

--- src/js/fileInput.js
+++ src/js/fileInput.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const fileInput = document.getElementById('file-input');
-  const label = document.getElementById('file-label');
+  const fileInput = document.getElementById("file-input");
+  const fileLabel = document.getElementById("file-label");
+  const form = document.getElementById("contacts");
 
-  fileInput.addEventListener('change', function() {
-    if (fileInput.files.length > 0) {
-      label.textContent = 'файл прикреплен';
-    } else {
-      label.textContent = 'прикрепить бриф';
-    }
+  form.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    fileLabel.classList.add("drag-over");
   });
+
+  form.addEventListener("dragleave", () => {
+    fileLabel.classList.remove("drag-over");
+  });
+
+  form.addEventListener("drop", (event) => {
+    event.preventDefault();
+    fileLabel.classList.remove("drag-over");
+
+    const files = event.dataTransfer.files;
+    fileInput.files = files;
+
+    const fileNames = Array.from(files).map(file => file.name).join(", ");
+    fileLabel.textContent = `Файлы: ${fileNames}`;
+  });
+
 });
