9ad06989c97fc302e59e8c6fec94a3a95208d8d0
Genrikh Antipov <genrikhantipov@gmail.com>
2024-11-07 18:02:34 +0300

    fix: change scroll script

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,16 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const anchorLinks = document.querySelectorAll('.nav__element');
-
-  anchorLinks.forEach(link => {
-      link.addEventListener('click', function(event) {
-          event.preventDefault();
-          const targetId = this.getAttribute('href');
-          const targetElement = document.querySelector(targetId);
-
-          if (targetElement) {
-              targetElement.scrollIntoView({ behavior: 'smooth' });
-              history.replaceState(null, null, window.location.href.split('#')[0]);
-          }
-      });
+$(document).ready(function() {
+    $('.nav__element').on('click', function(event) {
+      event.preventDefault();
+      
+      const targetId = $(this).attr('href');
+      const targetElement = $(targetId);
+  
+      if (targetElement.length) {
+        $('html, body').animate({
+          scrollTop: targetElement.offset().top
+        }, 1000);
+      }
+    });
   });
-});
